8fef02b873b489eac134f8e9dc13e606f447b786
hrytsiukpavlo <overdrive800392@gmail.com>
2022-08-27 15:56:43 +0300

    added fruits, updated routing
    added fruits, updated routing

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,13 @@
+import { Link } from "react-router-dom";
+import Store from "./Store";
+
 export default function About() {
-	return <h1>This is about page.</h1>;
+	return (
+		<>
+			<h1>This is about page.</h1>
+			<Link to="/store" style={{ fontSize: "2.5rem", textDecoration: "none" }}>
+				To visit the store click here
+			</Link>
+		</>
+	);
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,12 @@
+import { Link } from "react-router-dom";
+
 export default function Home() {
-	return <h1>This is home page.</h1>;
+	return (
+		<>
+			<h1>This is home page.</h1>
+			<Link to="/store" style={{ fontSize: "2.5rem", textDecoration: "none" }}>
+				To visit the store click here
+			</Link>
+		</>
+	);
 }
